f7771037e0721ce185ceda00ac4db7642479686c
Jacobtey <jacobtey@gmail.com>
2016-02-08 13:14:51 +0300

    Create sysinfo.sh

---
+++ contents/scripts/sysinfo.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+    echo -e " "
+    echo -e "`lsb_release -a | grep Description | cut -c 14-`" "`lsb_release -a | grep Release | cut -c 10-`" "`lsb_release -a | grep Codename | cut -c 11-`" "kernel `uname -r | cut -f 1-2 -d "-"`" "`uname -m`"
+    echo -e "plasma `plasmashell -v | grep plasmashel | cut -c 13-`" "kapp `dolphin -v | grep dolphin | cut -c 10-`" "kde `kf5-config -v | grep KDE | cut -c 17-`" "qt `kf5-config -v | grep Qt | cut -c 5-`"
+    echo -n "`uname -n`" "`uptime -p`" #if you want to uncomment strings below, you need change 'echo -n' to 'echo -e'
+#    echo -e "IPv6 `ifconfig teredo | grep 'inet6 2001' | cut -f 10 -d " "`"
+#    echo -n "IPv4 `wget -q -O - http://ip-api.com/json | cut -f 9 -d "," | cut -f 2 -d ":" | sed -e 's/"//g'`"
