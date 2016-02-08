ed62802519112c1026606b637f5bba135b4008c6
Jacobtey <jacobtey@gmail.com>
2016-02-08 16:22:36 +0300

    Update sysinfo1.sh

--- contents/scripts/sysinfo1.sh
+++ contents/scripts/sysinfo1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo -e "IPv6 `ifconfig teredo | grep 'inet6 2001' | cut -f 10 -d " "`"
-echo -n "IPv4 `wget -q https://api.ipify.org/`"
+echo -n "IPv4 `wget -q -O - https://api.ipify.org/`"
