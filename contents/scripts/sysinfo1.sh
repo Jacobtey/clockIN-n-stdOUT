1429412ef6396caef7f4e2479ed284db79f31964
Jacobtey <jacobtey@gmail.com>
2016-02-12 14:04:33 +0300

    Update sysinfo1.sh

--- contents/scripts/sysinfo1.sh
+++ contents/scripts/sysinfo1.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-echo -e "IPv6 `ifconfig teredo | grep 'inet6 2001' | cut -f 10 -d " "`"
-echo -n "IPv4 `wget -q -O - https://api.ipify.org/`"
+echo -n "`uname -n`" "`uptime -p`" #if you want to uncomment strings below, you need change 'echo -n' to 'echo -e'
+#echo -e "IPv6 `ifconfig teredo | grep 'inet6 2001' | cut -f 10 -d " "`"
+#echo -n "IPv4 `wget -q -O - https://api.ipify.org/`"
