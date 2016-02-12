63f850fe3621e9a2743f64ad79ca1439643697d4
Jacobtey <jacobtey@gmail.com>
2016-02-12 14:06:46 +0300

    Update and rename sysinfo1.sh to sysinfoUP.sh

--- contents/scripts/sysinfoUP.sh
+++ contents/scripts/sysinfoUP.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
 echo -n "`uname -n`" "`uptime -p`" #if you want to uncomment strings below, you need change 'echo -n' to 'echo -e'
-#echo -e "IPv6 `ifconfig teredo | grep 'inet6 2001' | cut -f 10 -d " "`"
-#echo -n "IPv4 `wget -q -O - https://api.ipify.org/`"
